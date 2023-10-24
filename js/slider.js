ea6e0bd01cec845cdd54521a5ea5dd5ea4ab170c
ThoNguyen <tientho2012004@gmail.com>
2023-10-24 21:43:25 +0700

    First Commit

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+showSlidesAuto();
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+function showSlidesAuto() {
+    let j;
+    let slidesAuto = document.getElementsByClassName("mySlides");
+    for (j = 0; j < slidesAuto.length; j++) {
+        slidesAuto[j].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slidesAuto.length) {slideIndex = 1}    
+    slidesAuto[slideIndex-1].style.display = "block";  
+    setTimeout(showSlidesAuto, 2000); // Change image every 2 seconds
+  }
